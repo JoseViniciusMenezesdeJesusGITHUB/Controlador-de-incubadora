c297d7cc0455106521fb2a5a10dc8599d1972225
José Vinicius <51296585+JoseViniciusMenezesdeJesusGITHUB@users.noreply.github.com>
2021-10-04 16:48:55 -0300

    Alterações no código fonte
    Hoje trabalhei iniciando a organização do código fonte do controlador, foi um trabalho lento pois tive dificuldade em usar o arquivo de cabeçalho. As alterações dentro do contexto de organização do código foi na criação de um cabeçalho responsável pelas configurações do display.

--- Controlador_de_temperatura_CODIGO/Controlador_de_temperatura_CODIGO.ino
+++ Controlador_de_temperatura_CODIGO/Controlador_de_temperatura_CODIGO.ino
@@ -1,6 +1,6 @@
 #include <TimerOne.h>
-#include <TM74HC595Display.h> //Modificar para atender minhas necessidades
 #include <Thermistor.h>
+#include "displayConfig.h"
 
 //Variaveis do Zero crossing
 int i = 0;
@@ -25,16 +25,6 @@ float somatorio = 0;
 float oldSomatorio = 0;
 
 
-//Variaveis do display
-int SCLK = 7;
-int RCLK = 6;
-int DIO = 5;
-
-
-TM74HC595Display disp(SCLK, RCLK, DIO);
-unsigned char LED_0F[30];
-
-
 //Variaveis de tempo
 long timer1 = 0, timer2 = 0;
 
@@ -45,38 +35,9 @@ float P = 0, I = 0, oldP = 0, saida = 0;
 
 
 void setup() {
-  
-  LED_0F[0] = 0xC0; //0
-  LED_0F[1] = 0xF9; //1
-  LED_0F[2] = 0xA4; //2
-  LED_0F[3] = 0xB0; //3
-  LED_0F[4] = 0x99; //4
-  LED_0F[5] = 0x92; //5
-  LED_0F[6] = 0x82; //6
-  LED_0F[7] = 0xF8; //7
-  LED_0F[8] = 0x80; //8
-  LED_0F[9] = 0x90; //9
-  LED_0F[10] = 0x88; //A
-  LED_0F[11] = 0x83; //b
-  LED_0F[12] = 0xC6; //C
-  LED_0F[13] = 0xA1; //d
-  LED_0F[14] = 0x86; //E
-  LED_0F[15] = 0x8E; //F
-  LED_0F[16] = 0xC2; //G
-  LED_0F[17] = 0x89; //H
-  LED_0F[18] = 0xF9; //I
-  LED_0F[19] = 0xF1; //J
-  LED_0F[20] = 0xC7; //L
-  LED_0F[21] = 0xA9; //n
-  LED_0F[22] = 0xC0; //O
-  LED_0F[23] = 0x8C; //P
-  LED_0F[24] = 0x98; //q
-  LED_0F[25] = 0x92; //S
-  LED_0F[26] = 0xC1; //U
-  LED_0F[27] = 0x91; //Y
-  LED_0F[28] = 0xFE; //hight -
-  LED_0F[29] = 0x7F; //.
 
+  LED_OF_config();
+  
   pinMode(triac, OUTPUT);
   attachInterrupt(0, cruzamentoZero, RISING);
   pinMode(motor, OUTPUT);
